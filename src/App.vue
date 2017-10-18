4d3f872a8dc20ece8e5ac7133140c9f47bb145f9
lunzhang <lun.zhang1993@gmail.com>
2017-10-18 15:24:01 -0400

    added grid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div v-for="(row, rowNum) in board">
+      <Cell v-for="(state, colNum) in row" :key="rowNum + ',' + colNum" :color="colors[state]"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Cell from './components/Cell';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      board: [[], [], [], [], [], [], [], [], [], []],
+      // white red blue yellow green purple grey
+      colors: ['#ffffff', '#FFCDD2', '#C5CAE9', '#FFF9C4', '#C8E6C9', '#D1C4E9', '#F5F5F5'],
+      currentColors: [1, 2, 3],
+    };
+  },
   components: {
-    HelloWorld,
+    Cell,
+  },
+  created() {
+    this.initBoard();
+  },
+  methods: {
+    initBoard() {
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          const color = Math.floor(Math.random() * this.currentColors.length);
+          this.board[i][j] = this.currentColors[color];
+        }
+      }
+      console.log(this.board);
+    },
   },
 };
 </script>
---
+++ src/components/Cell.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cell" v-bind:style="{ backgroundColor: this.color}"></div>
+</template>
+
+<script>
+export default {
+  name: 'Cell',
+  props: ['color'],
+  created() {
+    console.log(this.color);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cell {
+  width: 25px;
+  height: 25px;
+  display: inline-block;
+  margin: 1px;
+  border-radius: 5px;
+}
+</style>
