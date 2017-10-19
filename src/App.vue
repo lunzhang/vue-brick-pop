c74b89a62e489b23156926361771c093154a0447
lunzhang <lun.zhang1993@gmail.com>
2017-10-19 10:51:50 -0400

    [feature] popping of bricks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
     <div v-for="(row, rowNum) in board">
-      <Cell v-for="(state, colNum) in row" :key="rowNum + ',' + colNum" :color="colors[state]"/>
+      <div v-for="(color, colNum) in row" v-on:click="pop(rowNum, colNum)"
+      class="brick" v-bind:style="{ backgroundColor: colors[color]}"/>
     </div>
   </div>
 </template>
 
 <script>
-import Cell from './components/Cell';
-
 export default {
   name: 'app',
   data() {
@@ -17,10 +16,13 @@ export default {
       // white red blue yellow green purple grey
       colors: ['#ffffff', '#FFCDD2', '#C5CAE9', '#FFF9C4', '#C8E6C9', '#D1C4E9', '#F5F5F5'],
       currentColors: [1, 2, 3],
+      poppedRange: {
+        minVert: 0,
+        maxVert: 0,
+        minHorz: 0,
+        maxHorz: 0,
+      },
     };
-  },
-  components: {
-    Cell,
   },
   created() {
     this.initBoard();
@@ -35,6 +37,40 @@ export default {
       }
       console.log(this.board);
     },
+    pop(rowNum, colNum) {
+      if (this.board[rowNum][colNum] !== 0) {
+        const initColor = this.board[rowNum][colNum];
+        this.poppedRange.minVert = rowNum;
+        this.poppedRange.maxVert = rowNum;
+        this.poppedRange.minHorz = colNum;
+        this.poppedRange.maxHorz = colNum;
+        this.traversePop(rowNum, colNum, initColor);
+        this.tidyBoard();
+      }
+    },
+    traversePop(rowNum, colNum, initColor) {
+      // replace popped brick with 0
+      this.board[rowNum].splice(colNum, 1, 0);
+
+      // get top, bottom, right, left, bricks
+      const top = this.board[rowNum + 1] ? this.board[rowNum + 1][colNum] : null;
+      const bottom = this.board[rowNum - 1] ? this.board[rowNum - 1][colNum] : null;
+      const right = this.board[rowNum][colNum + 1] ? this.board[rowNum][colNum + 1] : null;
+      const left = this.board[rowNum][colNum - 1] ? this.board[rowNum][colNum - 1] : null;
+
+      // recursive pop same color bricks
+      if (top === initColor) this.traversePop(rowNum + 1, colNum, initColor);
+      if (bottom === initColor) this.traversePop(rowNum - 1, colNum, initColor);
+      if (right === initColor) this.traversePop(rowNum, colNum + 1, initColor);
+      if (left === initColor) this.traversePop(rowNum, colNum - 1, initColor);
+
+      if (rowNum > this.poppedRange.maxVert) this.poppedRange.maxVert = rowNum;
+      if (rowNum < this.poppedRange.minVert) this.poppedRange.minVert = rowNum;
+      if (colNum > this.poppedRange.maxHorz) this.poppedRange.maxHorz = colNum;
+      if (colNum < this.poppedRange.minHorz) this.poppedRange.minHorz = colNum;
+    },
+    tidyBoard() {
+    },
   },
 };
 </script>
@@ -47,5 +83,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#app .brick {
+  width: 25px;
+  height: 25px;
+  display: inline-block;
+  margin: 1px;
+  border-radius: 5px;
 }
 </style>
